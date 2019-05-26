e52fca034bf1e810813cf6b90f7b1e82e2898ebc
Alan Denniston <alan.denniston@gmail.com>
2019-05-25 23:44:16 -0400

    Update start.sh
    typo

--- docker/opt/start.sh
+++ docker/opt/start.sh
@@ -141,8 +141,7 @@ echo "Extracting files from the archive"
 tar xzvf /opt/zimbra-install/zimbra.tar.gz -C /opt/zimbra-install/
 
 echo "Installing Zimbra Collaboration just the Software"
-cd /opt/zimbra-install/zcs-* && ./install.sh -s < /opt/zimbra-install/installZimbra-keystrokesmkdir -p /opt/zimbra/common/lib/jvm/java/jre/lib/security
-chown -R zimbra:zimbra /opt/zimbra/common/lib/jvm/java/jre/lib/security
+cd /opt/zimbra-install/zcs-* && ./install.sh -s < /opt/zimbra-install/installZimbra-keystrokes
 
 # Work around install issues.
 mkdir -p /opt/zimbra/common/lib/jvm/java/jre/lib/security
